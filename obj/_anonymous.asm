25668920e95ed68afaf2443da3553cb323078f59
Lustgarten <leolust@outlook.fr>
2024-04-11 17:58:53 +0200

    Debut de traduction des exepressions

--- obj/_anonymous.asm
+++ obj/_anonymous.asm
@@ -1,7 +1,45 @@
 global _start
 section .text
 _start:
-;ceci est un test
+push -2
+push 4
+push 2
+pop r8
+pop r9
+add r8, r9
+push r8
+push 2
+push 5
+push 2
+pop r9
+pop r8
+sub r8, r9
+push r8
+push 2
+push 2
+pop r8
+pop r9
+imul r8, r9
+push r8
+push 1
+pop r8
+mov rdx, 0
+pop rax
+idiv r8
+push rax
+push 3
+push 1
+pop r8
+pop r9
+add r8, r9
+push r8
+push 122
+push 11
+pop r8
+mov rdx, 0
+pop rax
+idiv r8
+push rdx
 mov rax, 60
 mov rdi, 0
 syscall
--- src/traducteur.h
+++ src/traducteur.h
@@ -8,7 +8,19 @@ int write_start(FILE * anonymous);
 
 int write_end(FILE * anonymous);
 
-int write_sub(FILE * anonymous);
+int write_add(FILE * anonymous, Node *node);
+
+int write_sub(FILE * anonymous, Node *node);
+
+int write_mul(FILE * anonymous, Node *node);
+
+int write_div(FILE * anonymous, Node *node);
+
+int write_mod(FILE * anonymous, Node *node);
+
+int write_number(FILE * anonymous, int val);
+
+int eval_expr(FILE * anonymous, Node * node);
 
 void cToAsm(Node *node, FILE * file);
 
